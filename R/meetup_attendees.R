2b9ea91e5d06d8ef069da889e9f321285d92ed8a
Shelmith-Kariuki <kariukishelmith@gmail.com>
2020-10-10 21:01:56 +0300

    Added a dataset to the package

---
+++ R/meetup_attendees.R
@@ -0,0 +1,16 @@
+#' meetup_attendees
+#'
+#' This data contains information about some attendees of the meetup
+#' @docType data
+#'
+#' @usage data(meetup_attendees)
+#'
+#' @format A data frame with 3 variables:
+#' \describe{
+#' \item{\code{first_name}}{First name}
+#' \item{\code{last_name}}{Last _name}
+#' \item{\code{country}}{Country where the attendee is based}
+#'}
+#' @keywords datasets
+#'
+"meetup_attendees"
