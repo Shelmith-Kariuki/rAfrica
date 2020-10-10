d966c13185aacf3c213c8d787f2a9b4d6da60695
Shelmith-Kariuki <kariukishelmith@gmail.com>
2020-10-10 19:53:31 +0300

    Initial commit

---
+++ R/jumla.R
@@ -0,0 +1,17 @@
+#' jumla
+#' takes in two numbers, and adds them.
+#'
+#' @param x The first argument
+#'
+#' @param y The second argument
+#'
+#' @return a value that is the summation of x and y
+#' @export
+#'
+#' @examples
+#' summation <- jumla(10,20)
+#'
+jumla <- function(x,y){
+  x + y
+}
+
